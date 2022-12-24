4b891fca7e6199088941d7cbc9b81d45913ee78c
jhpark-mac <ggoowlgns@gmail.com>
2022-12-24 18:08:52 +0900

    init project

---
+++ src/main/java/com/jhpark/psearcher/PlaceSearcherWebappApplication.java
@@ -0,0 +1,13 @@
+package com.jhpark.psearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSearcherWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlaceSearcherWebappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jhpark/psearcher/PlaceSearcherWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.psearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceSearcherWebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
